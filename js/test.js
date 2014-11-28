32b207da63cb0204c170f7f4a97cfc9b86336f29
kody smith <kodysmith2010@gmail.com>
2014-11-27 17:57:57 -0800

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,8 @@
+// example code
+function characterMatrix(characterNumber){
+  var characterArray = array('a','b','c','d','e','f','g','h','i','j');
+  return characterArray[characterNumber];
+}
+//jQuery
+$('#line1').append(characterMatrix(rand(1,10)));
+
