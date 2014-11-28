5a54df55eaeb7d04bdcd2b9192dc3f944288c6f7
kody <kody@kodysmith.me>
2014-11-27 22:59:27 -0800

    adding model updates

--- js/models/verticalAnimatedLine.js
+++ js/models/verticalAnimatedLine.js
@@ -1,1 +1,30 @@
 // this file should contain the model code for vertical animated lines
+var verticalLine = function(position,size,speed){
+	var characterArray = Array('a','b','c','d','e','f','g','h','i','j');
+	var element = document.createElement('div');
+
+	function _display(position, size){
+		element.className = size;
+		element.style="position:absolute; left:"+position;
+		document.body.appendChild(element);
+	}
+	function _addCharacter(character){
+		element.innerHTML+=character;
+	}
+	function remove(){
+
+	}
+
+	function _start(){
+
+	}
+
+	function stop(){
+
+	}
+	
+	return {
+		display: function(position,size,speed){_display(position,size); _start(speed);},
+		addChar: function(character){_addCharacter(character);}
+	}
+}
