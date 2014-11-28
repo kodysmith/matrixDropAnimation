d288130871d31be532ddb67bcb7b2b9a5b7b4cfc
smithrk <ryan.smith@coatinginspectors.com>
2014-11-28 00:33:56 -0600

    corrected index.html

--- js/test.js
+++ js/test.js
@@ -2,8 +2,8 @@
   var characterArray = Array('a','b','c','d','e','f','g','h','i','j');
   var myDiv = document.getElementsByTagName ("div")[0];
   function randomInt(){
-  	return Math.round(Math.random(10), 1);
+  	return Math.round(Math.random()*10, 1);
   }
 //jQuery
-$('#line1').append(characterMatrix(rand(1,10)));
+//$('#line1').append(characterMatrix(rand(1,10)));
 
