c4b93720acb3451d648ddf285ac07a5338911e4a
kody <kody@kodysmith.me>
2014-11-30 18:51:20 -0800

    adding matrix function

--- js/test.js
+++ js/test.js
@@ -13,3 +13,26 @@
 		}
 	}
 }
+
+
+function matrix(lines){
+
+	var linesArray = Array();
+	setTimeout(function(){
+		linesArray[i] = new verticalLine();
+		var randomPosition = Math.floor(Math.random() * 1000);
+		var randomSpeed = Math.floor(Math.random() * 1000);
+		var randomSize = Math.floor(Math.random());
+		linesArray[i].display(randomPosition,randomSize,randomSpeed);
+	},Math.floor(Math.random() * 1000));
+		/*
+		for(i=0; i<lines; i++){
+			linesArray[i] = new verticalLine();
+			var randomPosition = Math.floor(Math.random() * 1000);
+			var randomSpeed = Math.floor(Math.random() * 1000);
+			var randomSize = Math.floor(Math.random());
+			linesArray[i].display(randomPosition,randomSize,randomSpeed);
+
+		}
+		*/
+	}
