afc3f3efaf90d0662e9e8dcfcb10bec348b9515f
smithrk <ryan.smith@coatinginspectors.com>
2014-11-30 21:05:49 -0600

    added timeout to test.js

--- js/test.js
+++ js/test.js
@@ -2,20 +2,25 @@
  document.onreadystatechange = function () { 
  	if (document.readyState == "complete") { //document is ready. Do your stuff here } }
 		var linesArray = [];
+		var position;
+		var size;
+		var speed;
+		var arrayPosition;
 		for (var i = 0; i < 20; i++) {
-			var position = Math.floor(Math.random()*1000);
-			var size = Math.floor(Math.random()*1000);
-			var speed = Math.floor(Math.random()*1000);
-			linesArray[i] = new verticalLine();
+			position = Math.floor(Math.random()*1000);
+			size = Math.floor(Math.random()*1000);
+			speed = Math.floor(Math.random()*1000);
+			arrayPosition = i;
+			linesArray[arrayPosition] = new verticalLine();
 			setTimeout(function(){
-				linesArray[i].display(position, size, speed);
+				linesArray[arrayPosition].display(position, size, speed);
 			},Math.random()*2000);
 		}
 	}
 }
 
 
-function matrix(lines){
+/*function matrix(lines){
 
 	var linesArray = Array();
 	setTimeout(function(){
@@ -25,7 +30,7 @@ function matrix(lines){
 		var randomSize = Math.floor(Math.random());
 		linesArray[i].display(randomPosition,randomSize,randomSpeed);
 	},Math.floor(Math.random() * 1000));
-		/*
+		
 		for(i=0; i<lines; i++){
 			linesArray[i] = new verticalLine();
 			var randomPosition = Math.floor(Math.random() * 1000);
@@ -34,5 +39,6 @@ function matrix(lines){
 			linesArray[i].display(randomPosition,randomSize,randomSpeed);
 
 		}
-		*/
+		
 	}
+*/
