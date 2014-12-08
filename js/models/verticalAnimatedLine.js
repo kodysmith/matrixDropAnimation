328ff9c83ffb0d16c92c9667a9cdedd190015d0d
smithrk <ryan.smith@coatinginspectors.com>
2014-12-07 22:13:16 -0600

    Update verticalAnimatedLine.js

--- js/models/verticalAnimatedLine.js
+++ js/models/verticalAnimatedLine.js
@@ -5,18 +5,19 @@ function randomRange(min, max) {
 var verticalLine = function(position,size,speed){
 	var options = {
 					'animationElement':'divMatrix',
-					'depthSizeRange':Array(2,2)
+					'depthSizeRange':Array(2,2),
+					'maxVerticalLines': 40,
+					'oldLettersElement' = document.createElement('span'),
+	 				'positionX' = randomRange(window.innerWidth, -10),
+		 			'positionY' = randomRange(window.innerHeight/2, -10),
+	 				'size' = randomRange(options.depthSizeRange[0],options.depthSizeRange[1]),
+	 				'speed' = randomRange(100, 650)
 					}
 	var stockCharacterArray = "0123456789!@#$%^&*(_+-=~`0123456789!@#$%^&*(_+-=~`0123456789!@#$%^&*(_+-=~`0123456789!@#$%^&*(_+-=~`0123456789!@#$%^&*(_+-=~`";
 	var randomCharacterArray = Array ();
 	var element = document.createElement('div');
 	var newLetterElement = document.createElement('span');
 	    newLetterElement.className = " newLetter";
-	var oldLettersElement = document.createElement('span');
-	var positionX = randomRange(window.innerWidth, -10);
-	var positionY = randomRange(window.innerHeight/2, -10);
-	var size = randomRange(options.depthSizeRange[0],options.depthSizeRange[1]);
-	var speed = randomRange(100, 650);
 	function _display(){
 		element.style.fontSize = size + "em";
 		element.style.width = size*10 + "px"
@@ -40,6 +41,8 @@ var verticalLine = function(position,size,speed){
 
 	function _start(speed){
 		setTimeout(function(){
+			var numberOfLines = element.parentElement.childNodes.length;
+			if (numberOfLines > options.maxVerticalLines) {
 			if (element.innerHTML.length < randomRange(20,1600)) {
 				_addCharacter(stockCharacterArray[Math.floor(randomRange(0,stockCharacterArray.length))] + "\n");
 				_start(speed);
